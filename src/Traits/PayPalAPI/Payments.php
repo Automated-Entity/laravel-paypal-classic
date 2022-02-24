17aaf8569a1d79c8a24d4f5cd1ef0970805cea34
Matthew von Glahn <vongles@gmail.com>
2022-02-24 18:53:35 -0500

    Just one or two quick changes.

--- src/Traits/PayPalAPI/Payments.php
+++ src/Traits/PayPalAPI/Payments.php
@@ -17,11 +17,12 @@ trait Payments
 	 */
 	public function listBillingPlans(int $page = 1, int $size = 20, bool $totals = true)
 	: \Psr\Http\Message\StreamInterface|array|string {
-		$this->apiEndPoint = "v1/payments/billing-plans?page={$page}&page_size={$size}&total_required=".($totals) ? 'true' : 'false';
+		$total_required = ($totals) ? 'yes' : 'no';
+		$this->apiEndPoint = "v1/payments/billing-plans?page={$page}&page_size={$size}&total_required={$total_required}";
 
 		$this->verb = 'get';
 
-		return $this->doPayPalRequest();
+		return $this->doPayPalRequest(false);
 	}
 
 }
